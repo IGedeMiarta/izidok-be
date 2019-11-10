5ee6eb24bd801479012ed2b29fbcbb4ceb926a28
Rezpa Aditya <rezpaditya@Rezpas-MacBook-Pro.local>
2019-11-10 22:17:32 +0700

    progress crud transaksi klinik

--- app/Http/Controllers/TransKlinikController.php
+++ app/Http/Controllers/TransKlinikController.php
@@ -3,82 +3,91 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TransKlinik;
 
 class TransKlinikController extends Controller 
 {
-
-  /**
-   * Display a listing of the resource.
-   *
-   * @return Response
-   */
   public function index()
   {
-    
+    $trans_klinik = TransKlinik::all()->paginate($request->limit);
+        
+        if (!$trans_klinik) {
+            return response()->json(['status' => false]);
+        }else{
+            return response()->json(['status' => true, 'data' => $trans_klinik]);
+        }
   }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create()
-  {
-    
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-   */
   public function store(Request $request)
   {
-    
+    $this->validate($request,[
+      'no_rekam_medis' => 'required|unique:rekam_medis|integer',
+      'no_ktp' => 'required|string',
+      'pasien_id' => 'required|pasien',
+      'jenis_kelamin' => 'required|integer|min:0|max:1',
+      'nomo_telp' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:8|max:12',
+      'dokter_id' => 'required|dokter',
+      'waktu_konsultasi' => 'required|string',
+
+      'tinggi_badan' => 'integer',
+      'berat_badan' => 'integer',
+      'suhu_badan' => 'integer',
+      'sistole' => 'integer',
+      'diastole' => 'integer',
+      'nadi' => 'integer',
+    ]);
+
+      $trans_klinik = new TransKlinik;
+      $trans_klinik->klinik_dokter_id = $request->trans_dokter_id;
+      $trans_klinik->pasien_id = $request->pasien_id;
+      $trans_klinik->nomor_antrian = $request->nomor_antrian;
+      $trans_klinik->klinik_operator_id = $request->klinik_operator_id;
+      $trans_klinik->klinik_id = $requst->klinik_id;
+      $trans_klinik->save();
+
+      if(!$trans_klinik){
+        return response()->json(['status' => false], 422);
+      }
+      
+      return response()->json([ 'status' => true, 
+                                'message' => 'data transaction has been saved successfully',
+                                'data' => $trans_klinik]);
+
   }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
   public function show($id)
   {
-    
+    $trans_klinik = TransKlinik::all()->find($id);
+    if (!$trans_klinik) {
+        return response()->json(['status' => false]);
+    }else{
+        return response()->json(['status' => true, 'data' => $trans_klinik]);
+    }
   }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function edit($id)
-  {
-    
-  }
-
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
   public function update($id)
   {
-    
+    $trans_klinik = TransKlinik::find($request->id);
+      if (!$trans_klinik) {            
+          return response()->json(['status' => false]);
+      }else{
+          $trans_klinik->status = $request->status;
+          $trans_klinik->save();
+          return response()->json(['status' => true, 'data' => $trans_klinik]);
+      }
   }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id)
+  public function delete($id = null)
   {
-    
+    $trans_klinik = TransKlinik::find($id);
+
+    if (!$trans_klinik) {            
+        return response()->json(['status' => false]);
+    }else{
+        $trans_klinik_id = $trans_klinik->id;
+        $trans_klinik->delete();
+        return response()->json(['status' => true, 'message' => 'Transaksi ID \''.$trans_klinik_id.'\' has been deleted']);
+    }
   }
   
 }
