04ce79edb0559532492ffe59046709592f0a3a2d
satriahelmy <helmysmp@gmail.com>
2019-11-25 09:01:51 +0700

    [update] update url reference

--- database/seeds/ReferenceTableSeeder.php
+++ database/seeds/ReferenceTableSeeder.php
@@ -13,7 +13,7 @@ class ReferenceTableSeeder extends Seeder
     public function run()
     {
     	$data_reference = array(
-    		array('id'=>'1', 'key'=> 'forgot_valid','value'=> 'localhost/forgot_password/valid','category'=> 'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
+    		array('id'=>'1', 'key'=> 'forgot_valid','value'=> 'http://149.129.239.15:8000/forgot-password/','category'=> 'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
             array('id'=>'2', 'key'=> 'forgot_invalid','value'=> 'localhost/forgot_password/invalid','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
             array('id'=>'3', 'key'=> 'activation_success','value'=> 'localhost/verification/success','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
             array('id'=>'4', 'key'=> 'activation_failed','value'=> 'localhost/verification/failed','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
