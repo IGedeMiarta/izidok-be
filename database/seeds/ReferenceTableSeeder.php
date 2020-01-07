fd83906fc7986d7ddd06101283ac4b057edac091
Rezpa Aditya <rezpaditya@Rezpas-MacBook-Pro.local>
2020-01-07 20:36:25 +0700

    update seeder

--- database/seeds/ReferenceTableSeeder.php
+++ database/seeds/ReferenceTableSeeder.php
@@ -13,16 +13,16 @@ class ReferenceTableSeeder extends Seeder
     public function run()
     {
     	$data_reference = array(
-    		array('id'=>'1', 'key'=> 'forgot_valid','value'=> 'http://149.129.239.15:8000/forgot-password/','category'=> 'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
-            array('id'=>'2', 'key'=> 'forgot_invalid','value'=> 'localhost/forgot_password/invalid','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
+    		array('id'=>'1', 'key'=> 'forgot_valid','value'=> 'http://izidok.id/forgot-password/','category'=> 'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
+            array('id'=>'2', 'key'=> 'forgot_invalid','value'=> 'http://izidok.id/forgot_password/invalid','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
         
-            array('id'=>'3', 'key'=> 'activation_success','value'=> 'http://149.129.239.15:8000/verification/success','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
-            array('id'=>'4', 'key'=> 'activation_failed','value'=> 'http://149.129.239.15:8000/verification/failed','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
+            array('id'=>'3', 'key'=> 'activation_success','value'=> 'http://izidok.id/verification/success','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
+            array('id'=>'4', 'key'=> 'activation_failed','value'=> 'http://izidok.id/verification/failed','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
             array('id'=>'5', 'key'=> 'verify_email','value'=> '/activate','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
-            array('id'=>'6', 'key'=> 'already_activated','value'=> 'http://149.129.239.15:8000/verification/already-activated','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
+            array('id'=>'6', 'key'=> 'already_activated','value'=> 'http://izidok.id/verification/already-activated','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
     	
-            array('id'=>'7', 'key'=> 'act_opt_valid','value'=> 'http://149.129.239.15:8000/verification/operator/','category'=> 'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
-            array('id'=>'8', 'key'=> 'act_opt_invalid','value'=> 'localhost/activation_operator/invalid','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
+            array('id'=>'7', 'key'=> 'act_opt_valid','value'=> 'http://izidok.id/verification/operator/','category'=> 'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
+            array('id'=>'8', 'key'=> 'act_opt_invalid','value'=> 'http://izidok.id/activation_operator/invalid','category'=>'url_redirection','created_at'=>date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')),
     	);
 
         DB::table('reference')->insert($data_reference);
