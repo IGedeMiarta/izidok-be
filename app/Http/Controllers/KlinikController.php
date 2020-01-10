efc737ca92648b0e13aceb0c83a72ee2bbcd7e16
Rezpa Aditya <rezpaditya@ntb-wil-keu-m01.ntb.corp.pln.co.id>
2020-01-10 09:34:08 +0700

    check email and username if exist

--- app/Http/Controllers/KlinikController.php
+++ app/Http/Controllers/KlinikController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Klinik;
 use App\User;
@@ -17,11 +18,12 @@ class KlinikController extends Controller
         //nothing
     }
 
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $user = User::find($request->user_id);
 
         if ($user->hasRole(Constant::SUPER_ADMIN)) {
-            $klinik = Klinik::all()->paginate($request->limit); 
+            $klinik = Klinik::all()->paginate($request->limit);
             $data['klinik'] = $klinik;
             return response()->json(['status' => true, 'data' => $data]);
         }
@@ -30,22 +32,24 @@ class KlinikController extends Controller
         return response()->json(['status' => true, 'data' => $data]);
     }
 
-    public function show($id = null, Request $request){
+    public function show($id = null, Request $request)
+    {
         $klinik = Klinik::find($id);
-        
+
         if (!$klinik) {
             return response()->json(['status' => false, 'message' => 'Klinik not found...'], 422);
         }
-        
-        if($klinik->user->id !== $request->user_id){
+
+        if ($klinik->user->id !== $request->user_id) {
             return response()->json(['status' => false, 'message' => 'you have no access to this klinik...'], 422);
         }
-        
+
         return response()->json(['status' => true, 'data' => $klinik]);
     }
 
-    public function store(Request $request){
-         $rules = [
+    public function store(Request $request)
+    {
+        $rules = [
             'tipe_faskes' => 'required|min:1:max:2',
             'nama_klinik' => 'required|string',
             'nomor_telp' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:8|max:15',
@@ -58,34 +62,35 @@ class KlinikController extends Controller
 
         #cek email & username
         $em = User::where('email', $request->email)
-                    ->orWhere('username', $request->username)
-                    ->first();
-        
-        if($em){
-            if($em->activation->status == 1){
-                return response()->json(['status' => false, 'message' => 'Email or username already in used!']);
+            ->orWhere('username', $request->username)
+            ->get();
+
+        if ($em) {
+            foreach ($em as $item) {
+                if ($item->activation->status == 1) {
+                    return response()->json(['status' => false, 'message' => 'Email or username already in used!']);
+                }
             }
         }
 
         $activation_url = Reference::where('key', Constant::VERIFY_EMAIL)->first();
         $isKlinik = false;
-        if($request->tipe_faskes == Constant::TIPE_KLINIK){
+        if ($request->tipe_faskes == Constant::TIPE_KLINIK) {
             $rules['nama_pic'] = 'required|string';
             $isKlinik = true;
         }
-        $this->validate($request,$rules);
+        $this->validate($request, $rules);
 
 
-        if($isKlinik){
+        if ($isKlinik) {
             $nama_pic = $request->nama_pic;
             #fitur registrasi klinik belum tersedia
             return response()->json(['status' => false, 'message' => 'register klinik unavailable...'], 422);
-        }
-        else{
+        } else {
             $nama_pic = $request->nama_klinik;
             $nama_dokter = $request->nama_klinik;
         }
-        
+
         #data klinik
         $klinik = Klinik::create([
             'tipe_faskes' => $request->tipe_faskes,
@@ -97,21 +102,21 @@ class KlinikController extends Controller
 
         $klinik->kode_faskes = sprintf('%06d', $klinik->id);
         $klinik->save();
-            
+
         #data user
         $user = User::create([
-    		"username" => $request->username,
-    		"email" => $request->email,
-    		"password" => app('hash')->make($request->password),
-    		"nama" => $nama_pic,
+            "username" => $request->username,
+            "email" => $request->email,
+            "password" => app('hash')->make($request->password),
+            "nama" => $nama_pic,
             "no_telp" => $request->nomor_telp,
             "klinik_id" => $klinik->id,
             "alamat" => $request->alamat,
-            "foto_profile" => \uploadToMinio('foto_profile',$request->foto_profile)
+            "foto_profile" => \uploadToMinio('foto_profile', $request->foto_profile)
         ]);
         $user->assignRole(Constant::DOKTER_PRAKTEK);
-        
-        if(!$isKlinik){
+
+        if (!$isKlinik) {
             #data dokter
             Dokter::create([
                 'nama' => $nama_dokter,
@@ -129,7 +134,7 @@ class KlinikController extends Controller
 
         $data['klinik_id'] = $klinik->id;
         $data['user_id'] = $user->id;
-        $data['activation_url'] =  url(env('APP_PREFIX', 'api/v1').$activation_url->value.'/'. $activation->token);       
+        $data['activation_url'] =  url(env('APP_PREFIX', 'api/v1') . $activation_url->value . '/' . $activation->token);
 
         $email_data = [
             'subject' => 'User Activation',
@@ -141,33 +146,35 @@ class KlinikController extends Controller
 
         \sendEmail($email_data, Constant::ACTIVATION_EMAIL_TEMPLATE);
 
-        if(!$data['klinik_id']){
+        if (!$data['klinik_id']) {
             return response()->json(['status' => false], 422);
         }
 
         return response()->json(['status' => true, 'data' => $data]);
     }
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
         $klinik = Klinik::find($request->id);
-        if (!$klinik) {            
+        if (!$klinik) {
             return response()->json(['status' => false, 'message' => 'klinik not found...']);
-        }else{
+        } else {
             $klinik->nama_klinik = $request->nama_klinik;
             $klinik->save();
             return response()->json(['status' => true, 'data' => $klinik]);
         }
     }
-    
-    public function delete($id = null){
+
+    public function delete($id = null)
+    {
         $klinik = Klinik::find($id);
 
-        if (!$klinik) {            
+        if (!$klinik) {
             return response()->json(['status' => false]);
-        }else{
+        } else {
             $nama = $klinik->nama_klinik;
             $klinik->delete();
-            return response()->json(['status' => true, 'message' => 'Klinik \''.$nama.'\' has been deleted']);
+            return response()->json(['status' => true, 'message' => 'Klinik \'' . $nama . '\' has been deleted']);
         }
     }
 }
