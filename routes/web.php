490ad885562ff31c37a5942cc8cfaf2d86f47470
helmy.satria <helmy.satria@pln.co.id>
2020-02-03 09:48:34 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -124,9 +124,8 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) {
             $router->get('/rekam_medis', 'RekamMedisController@index');
             $router->post('/rekam_medis', 'RekamMedisController@store');
             $router->get('/rekam_medis/{id}', 'RekamMedisController@show');
+            $router->get('/rekam_medis/date', 'RekamMedisController@getRekamMedisByTanggalLahir');
         });
-
-        $router->get('/rekam_medis/date', 'RekamMedisController@getRekamMedisByTanggalLahir');
 
         #organ
         $router->get('/organ/name', ['middleware' => 'permission:read-rekam-medis', 'uses' => 'OrganController@getByName']); // get by name
