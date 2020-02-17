680445f4246e551a042037058af6971603ef763d
rarinugraha <rizkyarinugraha.putra@gmail.com>
2020-02-17 15:51:03 +0700

    Fix redaction in email activation template

--- resources/views/email-activation.blade.php
+++ resources/views/email-activation.blade.php
@@ -100,19 +100,19 @@
                                             <div style="font-size: 12px; line-height: 1.5; font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; color: #555555; mso-line-height-alt: 18px;">
                                                 <p style="font-size: 14px; line-height: 1.5; mso-line-height-alt: 21px; margin: 0;">Aktifkan Akun izidok Anda!</p>
                                                 <p style="font-size: 14px; line-height: 1.5; mso-line-height-alt: 21px; margin: 0;"><br></p>
-                                                <p style="font-size: 14px; line-height: 1.5; mso-line-height-alt: 21px; margin: 0;">Kami telah menerima permintaan pembuatan akun Izidok untuk anda. Mohon konfirmasi email ini untuk mengaktifkan akun Izidok Anda.</p>
+                                                <p style="font-size: 14px; line-height: 1.5; mso-line-height-alt: 21px; margin: 0;">Kami telah menerima permintaan pembuatan akun izidok untuk Anda. Mohon konfirmasi email ini untuk mengaktifkan akun izidok Anda.</p>
                                             </div>
                                         </div>
                                         <!--[if mso]></td></tr></table><![endif]-->
                                         <div align="center" class="button-container" style="padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
                                             <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-spacing: 0; border-collapse: collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;"><tr><td style="padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px" align="center"><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="{{@$activation_url}}" style="height:31.5pt; width:382.5pt; v-text-anchor:middle;" arcsize="10%" stroke="false" fillcolor="#3AAEE0"><w:anchorlock></w:anchorlock><v:textbox inset="0,0,0,0"><center style="color:#ffffff; font-family:Arial, sans-serif; font-size:16px"><![endif]--><a href="{{@$activation_url}}" style="-webkit-text-size-adjust: none; text-decoration: none; display: block; color: #ffffff; background-color: #3AAEE0; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; width: 100%; width: calc(100% - 2px); border-top: 1px solid #3AAEE0; border-right: 1px solid #3AAEE0; border-bottom: 1px solid #3AAEE0; border-left: 1px solid #3AAEE0; padding-top: 5px; padding-bottom: 5px; font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; text-align: center; mso-border-alt: none; word-break: keep-all;"
-                                                target="_blank"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;">&nbsp;<span style="font-size: 16px; line-height: 2; mso-line-height-alt: 32px;">Konfirmasi e-mail</span>&nbsp;</span></a>
+                                                target="_blank"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;">&nbsp;<span style="font-size: 16px; line-height: 2; mso-line-height-alt: 32px;">Konfirmasi Email</span>&nbsp;</span></a>
                                             <!--[if mso]></center></v:textbox></v:roundrect></td></tr></table><![endif]-->
                                         </div>
                                         <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px; font-family: Arial, sans-serif"><![endif]-->
                                         <div style="color:#555555;font-family:'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif;line-height:1.2;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
                                             <div style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 12px; line-height: 1.2; color: #555555; mso-line-height-alt: 14px;">
-                                                <p style="font-size: 14px; line-height: 1.2; mso-line-height-alt: 17px; margin: 0;"><span style="color: #333333; font-size: 14px;">Jika anda
+                                                <p style="font-size: 14px; line-height: 1.2; mso-line-height-alt: 17px; margin: 0;"><span style="color: #333333; font-size: 14px;">Jika Anda
 kesulitan mengklik tombol &quot;Konfirmasi Email&quot;, copy dan paste
 URL di bawah ke dalam browser Anda:</span></p>
                                                 <p style="font-size: 14px; line-height: 1.2; mso-line-height-alt: 17px; margin: 0;"><span style="color: #0000ff; font-size: 14px;">{{@$activation_url}}</span></p>
@@ -137,11 +137,6 @@ URL di bawah ke dalam browser Anda:</span></p>
                                                             <td style="width: 8.8267%;">:</td>
                                                             <td style="width: 57.6641%;">{{@$email}}</td>
                                                         </tr>
-                                                        <tr>
-                                                            <td style="width: 33.3649%;">Password</td>
-                                                            <td style="width: 8.8267%;">:</td>
-                                                            <td style="width: 57.6641%;">{{@$password}}</td>
-                                                        </tr>
                                                     </tbody>
                                                 </table>
                                                 <p style="font-size: 14px; line-height: 1.2; mso-line-height-alt: 17px; margin: 0;"><br></p>
@@ -150,8 +145,7 @@ URL di bawah ke dalam browser Anda:</span></p>
 Kasih,</span></p>
                                                 <p style="font-size: 14px; line-height: 1.2; mso-line-height-alt: 17px; margin: 0;">&nbsp;</p>
                                                 <p style="font-size: 14px; line-height: 1.2; mso-line-height-alt: 17px; margin: 0;">&nbsp;</p>
-                                                <p style="font-size: 14px; line-height: 1.2; mso-line-height-alt: 17px; margin: 0;"><span style="color: #333333; font-size: 14px;">Izidok
-Team</span></p>
+                                                <p style="font-size: 14px; line-height: 1.2; mso-line-height-alt: 17px; margin: 0;"><span style="color: #333333; font-size: 14px;">Tim izidok</span></p>
                                             </div>
                                         </div>
                                         <!--[if mso]></td></tr></table><![endif]-->
