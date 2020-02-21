4a07b9cfaf8c9791a939662f3c5e0fab291f59fc
Dawai Pagi <abdie.dawaipagi@gmail.com>
2020-02-21 20:04:15 +0700

    update pasien

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -349,8 +349,8 @@ class PasienController extends Controller
 		$pasien->jenis_kelamin = $request->input('jenis_kelamin');
 		$pasien->golongan_darah = $request->input('golongan_darah');
 		$pasien->alamat_rumah = $request->input('alamat_rumah');
-		$pasien->provinsi = $request->input('provinsi');
-		$pasien->kota = $request->input('kota');
+		$pasien->provinsi = $request->provinsi ? $request->input('provinsi') : null;
+		$pasien->kota = $request->kota ? $request->input('kota') : null;
 		//$pasien->kelurahan = $request->input('kelurahan');
 		//$pasien->kecamatan = $request->input('kecamatan');
 		$pasien->status_perkawinan = $request->input('status_perkawinan');
