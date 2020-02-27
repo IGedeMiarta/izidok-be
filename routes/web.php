a1ca7b4464be1d429321183dec1d5b119193148a
rarinugraha <rizkyarinugraha.putra@gmail.com>
2020-02-27 16:22:55 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -107,8 +107,8 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) {
             $router->get('/layanan/{id}', 'LayananController@show');
             $router->put('/layanan/{id}', 'LayananController@update');
             $router->delete('/layanan/{id}', 'LayananController@delete');
-            $router->get('/layanan/{kode}/kode', 'LayananController@getByKode');
-            $router->get('/layanan/{nama}/nama', 'LayananController@getByNama');
+            $router->get('/layanan/kode/{kode}', 'LayananController@getByKode');
+            $router->get('/layanan/nama/{nama}', 'LayananController@getByNama');
         });
 
         //pasien
