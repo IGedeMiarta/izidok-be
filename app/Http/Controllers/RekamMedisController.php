8645dc5f111d5f4e25862d75764417be1ef84320
Dawai Pagi <abdie.dawaipagi@gmail.com>
2020-03-08 08:40:01 +0700

    update store rekam medis

--- app/Http/Controllers/RekamMedisController.php
+++ app/Http/Controllers/RekamMedisController.php
@@ -206,7 +206,7 @@ class RekamMedisController extends Controller
 
         $trans_klinik = TransKlinik::find($request->transklinik_id);
         $trans_klinik->durasi_konsultasi = $days;
-        $trans_klinik->tgl_next_konsultasi = $request->durasi_konsultasi == 1 ? date('Y-m-d') : date('Y-m-d', strtotime($request->tgl_next_konsultasi)); //1 = tidak memilih jadwal next konsultasi
+        $trans_klinik->tgl_next_konsultasi = $request->durasi_konsultasi == 99 ? date('Y-m-d') : date('Y-m-d', strtotime($request->tgl_next_konsultasi)); //99 = tidak memilih jadwal next konsultasi
         $trans_klinik->save();
 
         #get data pasien, insert anamnesa
