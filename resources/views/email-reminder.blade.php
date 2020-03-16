90371c935c885132aad7e38ea548929235277aa1
rarinugraha <rizkyarinugraha.putra@gmail.com>
2020-03-16 15:37:25 +0700

    Add email reminder template

---
+++ resources/views/email-reminder.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE">
+
+</head>
+<div class="content" style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px; width: 600px; margin-left: auto; margin-right: auto;">
+	<p style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: normal; padding: 0px 10px 0px 10px;">Hai, {{@$nama_pasien}}.</p>
+    <p style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: normal; padding: 0px 10px 0px 10px;">Berdasarkan konsultasi Anda sebelumnya dengan {{@$nama_dokter}} pada tanggal {{@$waktu_konsultasi}},
+        Anda dianjurkan untuk datang kembali pada tanggal {{@$next_konsultasi}}.</p>
+    <br style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;">
+    <p style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: normal; padding: 0px 10px 0px 10px;">Untuk informasi lebih lanjut, silahkan hubungi:</p>
+    <table style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px; width: 50%; border: 1px solid; border-collapse: collapse; border-color: gray; box-shadow: 1px 1px gray; margin-left: 10px;" width="50%">
+        <tr style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;">
+            <td style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px; height: 10px; padding: 5px 10px 10px 10px;" height="10">Praktik {{@$nama_dokter}}</td>
+        </tr>
+        <tr style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;">
+            <td style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px; height: 10px; padding: 5px 10px 10px 10px;" height="10">{{@$alamat}}</td>
+        </tr>
+        <tr style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;">
+           <td style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px; height: 10px; padding: 5px 10px 10px 10px;" height="10"> Telp. {{@$nomor_telp}}</td>
+        </tr>
+        <tr style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;">
+            <td style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px; height: 10px; padding: 5px 10px 10px 10px;" height="10">email: {{@$email}}</td>
+        </tr>
+    </table>
+    <br style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;">
+    <p style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: normal; padding: 0px 10px 0px 10px;">Terima kasih,<br style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;">Salam Sejahtera & Sehat Selalu</p>
+    <br style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;"><br style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px;">
+    <hr style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 14px; line-height: 6px; border-top: 1px solid black;">
+    <p class="footer" style="font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; line-height: normal; padding: 0px 10px 0px 10px; font-size: 12px; color: gray;">Email ini dibuat secara otomatis. Mohon untuk tidak mengirim balasan ke email ini.</p>
+</div>
+</html>
