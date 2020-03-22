8b1ce36d711897d14e9b721d6f67a922cc6d94cb
rarinugraha <rizkyarinugraha.putra@gmail.com>
2020-03-22 22:52:32 +0700

    Remove unused dd

--- app/Http/Controllers/PaketController.php
+++ app/Http/Controllers/PaketController.php
@@ -421,7 +421,6 @@ class PaketController extends Controller
                 where('klinik_id',$klinikId)
                 ->where('status',0)
                 ->where('expired_pay','>',date('Y-m-d H:i:s'));
-            //dd($billWaiting->exists());
             if ($billWaiting->exists()) {
                 //Saat inisiasi, memilih ‘beli paket’, lalu ketika statusnya sedang menunggu pembayaran, dia ingin akses menu lain.
                 return response()->json([
