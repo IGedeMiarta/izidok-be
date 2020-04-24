78d936e0060522105ba46973a8d9a9cc9f24e2f3
zaldyputraw@gmail.com <zaldyputraw123>
2020-04-24 15:42:14 +0700

    paygate bayarind

--- app/Http/Controllers/PaygateController.php
+++ app/Http/Controllers/PaygateController.php
@@ -217,7 +217,7 @@ class PaygateController extends Controller
             'transactionExpire' => $data['expired_pay'],
             'description' => $data['desc'],
             'callbackURL' => 'https://app.izidok.id/subskripsi',
-            'itemDetails' => $DetailsItem,
+            'itemDetails' => json_encode($DetailsItem),
             'customerAccount' => $custAcc,
             'customerName' => $user->nama,
             'authCode' => hash("sha256",$data['no_invoice'].$data['amount'].$pg->channel_id.$pg->secretkey)
